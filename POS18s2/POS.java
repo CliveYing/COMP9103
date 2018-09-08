cf92da30d70cb0e66c18111deb3d70cdfcb9e639
CliveYing <41674607+CliveYing@users.noreply.github.com>
2018-09-08 11:28:00 +1000

    Update POS.java

--- POS18s2/POS.java
+++ POS18s2/POS.java
@@ -3,8 +3,8 @@ package POS18s2;
 public class POS {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-      System.out.println("Hi");
+		// aaa
+        System.out.println("Hi");
 	}
 
 }
