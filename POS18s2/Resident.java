5c88e9f778249d691ef2c481cb24c87e0a6b0091
CliveYing <41674607+CliveYing@users.noreply.github.com>
2018-09-08 11:21:48 +1000

    2

---
+++ POS18s2/Resident.java
@@ -0,0 +1,13 @@
+package POS18s2;
+
+import java.util.ArrayList;
+
+public class Resident {
+	String name;
+    int[] postCode = new int[4];
+    int[] phone = new int[8];
+    ArrayList<String> petType = new ArrayList<String>();
+    
+    
+    
+}
